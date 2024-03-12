e7e560f0cdeb87068a92c3e30464ca2cd9c4a276
Deendayal423 <151893234+Deendayal423@users.noreply.github.com>
2024-03-12 06:15:15 +0530

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -1482,7 +1482,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             lazy_stream = f"{URL}watch/{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
             lazy_download = f"{URL}{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
 
-            xo = await query.message.reply_text(f'⚜️')
+            xo = await query.message.reply_text(f'🪔')
             await asyncio.sleep(1)
             await xo.delete()
 
