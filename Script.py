2fbef93f1f454cd83bf54ddb5b5773c38ee6f857
Deendayal423 <151893234+Deendayal423@users.noreply.github.com>
2024-03-25 15:57:08 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -235,7 +235,7 @@ Nᴀᴍᴇ : {}
 
 Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
-    CAPTION = """<b><a href="https://t.me/Deendayal_dhakad">{file_caption}</a></b>\n\n\n<b>⚜️ Powered By : <a href=https://t.me/Deendayal_dhakad>[Deendayal_Dhakad]</a></b>"""
+    CAPTION = """<b><a href="https://t.me/Deendayal_dhakad">{file_caption}</a></b>\n<b>⚜️ Powered By : <a href=https://t.me/Deendayal_dhakad>[Deendayal_Dhakad]</a></b>"""
 
     IMDB_TEMPLATE_TXT = """
 <b>🏷 Title</b>: <a href={url}>{title}</a>
