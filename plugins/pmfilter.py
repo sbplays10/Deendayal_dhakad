666dff8d32657ff12fd99ddaba95089338ea67ec
Deendayal423 <151893234+Deendayal423@users.noreply.github.com>
2024-04-17 22:15:00 +0530

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -1681,7 +1681,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         await client.edit_message_media(
             query.message.chat.id, 
             query.message.id, 
-            InputMediaPhoto(random.choice(PICS))
+            InputMediaPhoto(photo=REFER_IMG)
         )
         await query.message.edit_text(
             text=script.Deendayal_TXT.format(REFERAL_PREMEIUM_TIME, temp.U_NAME, query.from_user.id, REFERAL_COUNT),
