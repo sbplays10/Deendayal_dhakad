01f97b2f5c8781d8649efcc59305a29f19231cd3
Deendayal423 <151893234+Deendayal423@users.noreply.github.com>
2024-04-21 11:52:59 +0530

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -1675,13 +1675,14 @@ async def cb_handler(client: Client, query: CallbackQuery):
         )    
     elif query.data == "Dhakad":
         buttons = [[
+            InlineKeyboardButton('Invite', url=f'https://t.me/share/url?url=https://telegram.me/{temp.U_NAME}?start=Deendayal-{query.from_user.id}'),
             InlineKeyboardButton('⇚Back', callback_data='start')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await client.edit_message_media(
             query.message.chat.id, 
             query.message.id, 
-            InputMediaPhoto(random.choice(PICS))
+            InputMediaPhoto('https://graph.org/file/43729dd1e563f2595882f.jpg')
         )
         await query.message.edit_text(
             text=script.Deendayal_TXT.format(REFERAL_PREMEIUM_TIME, temp.U_NAME, query.from_user.id, REFERAL_COUNT),
