4ea7450d6309d4043ae24642d796f7be6c683edd
Deendayal423 <151893234+Deendayal423@users.noreply.github.com>
2024-04-28 22:06:57 +0530

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -310,7 +310,7 @@ async def advantage_spoll_choker(bot, query):
                 if NO_RESULTS_MSG:
                    await bot.send_message(chat_id=LOG_CHANNEL, text=(script.NORSLTS.format(reqstr.id, reqstr.mention, movie)))
                 k = await query.message.edit(text=script.MVE_NT_FND, reply_markup=InlineKeyboardMarkup(button))
-                await asyncio.sleep(10)
+                await asyncio.sleep(20)
                 await k.delete()
 #Qualities 
 @Client.on_callback_query(filters.regex(r"^qualities#"))
