5f77ebf39e9f028b161d4d47a8866201508de7cd
Deendayal423 <151893234+Deendayal423@users.noreply.github.com>
2024-05-03 08:57:08 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/HarshalPurohitEdits/TheMovieProviderBot.git /TheMovieProviderBot 
+  git clone https://github.com/Deendayal423/Super-Filter-bot.git /Super-Filter-bot 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /TheMoviesProviderBot
+  git clone $UPSTREAM_REPO /Super-Filter-bot
 fi
-cd /THALAPATHY-FILTER-BOT 
+cd /Super-Filter-bot
 pip3 install -U -r requirements.txt
-echo "Starting TheMovieProviderBot...."
+echo "Starting Super-Filter-bot...."
 python3 bot.py
