e05e871e348a8a306f572f837a21d85f19b15382
Deendayal423 <151893234+Deendayal423@users.noreply.github.com>
2024-06-17 12:11:33 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Deendayal423/Super-Filter-bot.git /Super-Filter-bot 
+  git clone https://github.com/Deendayal423/Deendayal_dhakad.git /Deendayal_dhakad 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Super-Filter-bot
+  git clone $UPSTREAM_REPO /Deendayal_dhakad
 fi
-cd /Super-Filter-bot
+cd /Deendayal_dhakad
 pip3 install -U -r requirements.txt
-echo "Starting Super-Filter-bot...."
+echo "Starting Deendayal_dhakad...."
 python3 bot.py
