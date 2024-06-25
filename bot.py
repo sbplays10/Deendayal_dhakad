fa1c2538cab7364ccb13b3e9d53dcf3580725aed
Deendayal423 <151893234+Deendayal423@users.noreply.github.com>
2024-06-25 19:33:57 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -32,13 +32,14 @@ from Script import script
 from datetime import date, datetime 
 import pytz
 from aiohttp import web
-from plugins import web_server, check_expired_premium 
+from plugins import web_server, check_expired_premium, watch_collection 
 
 import asyncio
 from pyrogram import idle
 from lazybot import LazyPrincessBot
 from util.keepalive import ping_server
 from lazybot.clients import initialize_clients
+LazyPrincessBot.loop.create_task(watch_collection(LazyPrincessBot))
 
 
 ppath = "plugins/*.py"
