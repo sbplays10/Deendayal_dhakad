7ff1a42b330031f9ae4e87a3705e86ae092209a7
Deendayal423 <151893234+Deendayal423@users.noreply.github.com>
2024-07-13 18:46:30 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -77,7 +77,7 @@ async def send_msg(bot, filename, caption):
 
         filename = filename.replace('(', '').replace(')', '').replace('[', '').replace(']', '').replace('{', '').replace('}', '').replace(':', '').replace(';', '').replace("'", '').replace('-', '').replace('!', '')
         
-        text = "#new_file_added ✅\n\n📍Name: `{}`\n\n🌳Quality: {}\n\n🦧Audio: {}"
+        text = "#new_file_added ✅\n\n👷Name:➥ `{}`\n\n🌳Quality: {}\n\n🍁Audio: {}"
         text = text.format(filename, quality, language)
         if await add_name(905710386, filename):
           imdb_task = get_movie_details(filename)
