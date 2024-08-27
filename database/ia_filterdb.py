ea020a2649b5221e0c924555dfe5257805baaec5
Deendayal_dhakad <151893234+Deendayal403@users.noreply.github.com>
2024-08-27 16:19:52 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -79,7 +79,7 @@ async def send_msg(bot, filename, caption):
 
         filename = filename.replace('(', '').replace(')', '').replace('[', '').replace(']', '').replace('{', '').replace('}', '').replace(':', '').replace(';', '').replace("'", '').replace('-', '').replace('!', '')
         
-        text = "#new_file_added ✅\n\n👷Name:➥ `{}`\n\n🌳Quality:➥ {}\n\n🍁Audio:➥ {}"
+        text = "#𝑵𝒆𝒘_𝑭𝒊𝒍𝒆_𝑨𝒅𝒅𝒆𝒅 ✅\n\n👷𝑵𝒂𝒎𝒆:➥ `{}`\n\n🌳𝑸𝒖𝒂𝒍𝒊𝒕𝒚:➥ {}\n\n🍁𝑨𝒖𝒅𝒊𝒐:➥ {}"
         text = text.format(filename, quality, language)
         if await add_name(6646028262, filename):
           imdb_task = get_movie_details(filename)
@@ -93,7 +93,7 @@ async def send_msg(bot, filename, caption):
                   resized_poster = await resized_poster_task
             
           filenames = filename.replace(" ", '-')
-          btn = [[InlineKeyboardButton('🌲 Search Here 🌲', url=f"https://t.me/Deendayal_Movie_Group")]]
+          btn = [[InlineKeyboardButton('🌲 𝑮𝒆𝒕 𝑭𝒊𝒍𝒆 🌲', url=f"https://telegram.me/{temp.U_NAME}?start=getfile-{filenames}")]]")]]
           if resized_poster:
               await bot.send_photo(chat_id=channel, photo=resized_poster, caption=text, reply_markup=InlineKeyboardMarkup(btn))
           else:              
