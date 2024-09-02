bdc7f8e7f4bbf54ebd5ee131aaee21c99b3c3583
Deendayal_dhakad <151893234+Deendayal403@users.noreply.github.com>
2024-09-02 13:49:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -62,7 +62,7 @@ DEENDAYAL_MOVIE_UPDATE_NOTIFICATION = bool(environ.get('DEENDAYAL_MOVIE_UPDATE_N
 
 
 # Verify
-VERIFY = bool(environ.get('VERIFY', True)) # Verification On ( True ) / Off ( False )
+VERIFY = bool(environ.get('VERIFY', False)) # Verification On ( True ) / Off ( False )
 DEENDAYAL_VERIFY_EXPIRE = int(environ.get('DEENDAYAL_VERIFY_EXPIRE', 12)) # Add time in hours
 HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', 'https://t.me/Deenu_dhakad') # How to open tutorial link for verification
 
